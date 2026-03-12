1d1a4095f058caa008e6442777b7fd33f8c0e203
Josimar Menegatt <164834998+josimarmenegatt86@users.noreply.github.com>
2026-03-12 03:24:46 +0000

    update

--- script.js
+++ script.js
@@ -6,61 +6,62 @@ let lastX = null
 let lastY = null
 
 const hands = new Hands({
- locateFile: file =>
- `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+locateFile: file =>
+`https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
 })
 
 hands.setOptions({
- maxNumHands:1,
- minDetectionConfidence:0.7,
- minTrackingConfidence:0.7
+maxNumHands:1,
+modelComplexity:1,
+minDetectionConfidence:0.7,
+minTrackingConfidence:0.7
 })
 
 hands.onResults(results=>{
 
- canvas.width = video.videoWidth
- canvas.height = video.videoHeight
+canvas.width = results.image.width
+canvas.height = results.image.height
 
- ctx.drawImage(results.image,0,0,canvas.width,canvas.height)
+ctx.drawImage(results.image,0,0,canvas.width,canvas.height)
 
- if(results.multiHandLandmarks){
+if(results.multiHandLandmarks){
 
- const landmarks = results.multiHandLandmarks[0]
+const landmarks = results.multiHandLandmarks[0]
 
- const indexFinger = landmarks[8]
+const index = landmarks[8]
 
- const x = indexFinger.x * canvas.width
- const y = indexFinger.y * canvas.height
+const x = index.x * canvas.width
+const y = index.y * canvas.height
 
- if(lastX && lastY){
+if(lastX && lastY){
 
- ctx.beginPath()
- ctx.moveTo(lastX,lastY)
- ctx.lineTo(x,y)
- ctx.strokeStyle="cyan"
- ctx.lineWidth=5
- ctx.stroke()
+ctx.beginPath()
+ctx.moveTo(lastX,lastY)
+ctx.lineTo(x,y)
+ctx.strokeStyle="cyan"
+ctx.lineWidth=5
+ctx.stroke()
 
- }
+}
 
- lastX = x
- lastY = y
+lastX = x
+lastY = y
 
- }else{
+}else{
 
- lastX=null
- lastY=null
+lastX=null
+lastY=null
 
- }
+}
 
 })
 
 const camera = new Camera(video,{
- onFrame: async ()=>{
- await hands.send({image:video})
- },
- width:640,
- height:480
+onFrame: async ()=>{
+await hands.send({image:video})
+},
+width:640,
+height:480
 })
 
 camera.start()
