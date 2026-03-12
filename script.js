ecac0f0111fff0f748cbd278c62308c066a9eba2
Josimar Menegatt <164834998+josimarmenegatt86@users.noreply.github.com>
2026-03-12 03:34:27 +0000

    update

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
-body{
-background:#111;
-color:white;
-font-family:Arial;
-text-align:center;
+const video = document.getElementById("video")
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+let trail=[]
+
+const hands = new Hands({
+locateFile: file =>
+`https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`
+})
+
+hands.setOptions({
+maxNumHands:1,
+minDetectionConfidence:0.7,
+minTrackingConfidence:0.7
+})
+
+hands.onResults(results=>{
+
+canvas.width = results.image.width
+canvas.height = results.image.height
+
+ctx.drawImage(results.image,0,0)
+
+if(results.multiHandLandmarks){
+
+const finger = results.multiHandLandmarks[0][8]
+
+const x = finger.x * canvas.width
+const y = finger.y * canvas.height
+
+trail.push({x,y})
+
+if(trail.length>30){
+trail.shift()
 }
 
-h1{
-margin-top:10px;
+ctx.lineJoin="round"
+ctx.lineCap="round"
+
+for(let i=1;i<trail.length;i++){
+
+ctx.beginPath()
+
+ctx.moveTo(trail[i-1].x,trail[i-1].y)
+ctx.lineTo(trail[i].x,trail[i].y)
+
+ctx.strokeStyle="cyan"
+ctx.lineWidth=8
+
+ctx.shadowBlur=20
+ctx.shadowColor="cyan"
+
+ctx.stroke()
+
 }
 
-.container{
-position:relative;
-display:inline-block;
 }
 
-canvas{
-border:3px solid cyan;
-border-radius:10px;
-max-width:900px;
-}
+})
 
-video{
-display:none;
-}
+navigator.mediaDevices.getUserMedia({video:true})
+.then(stream=>{
 
-.toolbar{
-margin:10px;
-}
+video.srcObject=stream
 
-button{
-padding:10px 20px;
-margin:5px;
-font-size:16px;
-border:none;
-border-radius:6px;
-cursor:pointer;
-}
+const camera=new Camera(video,{
+onFrame: async ()=>{
+await hands.send({image:video})
+},
+width:640,
+height:480
+})
 
-button:hover{
-background:#444;
-color:white;
-}
+camera.start()
 
-.info{
-margin-top:10px;
-color:#ccc;
-}
+})
